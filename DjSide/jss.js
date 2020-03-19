cc14d81f184b0744b3c26f4f62f730b685ec2617
WKrzysiek-repo <wojtekkrzysiek@gmail.com>
2020-03-19 15:25:28 +0100

    fix rem

--- DjSide/jss.js
+++ DjSide/jss.js
@@ -1,6 +1,18 @@
 let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
+
+
+var top_Button = document.querySelector('top1');
+
+$('.top1').on('click', function () {
+    $('body,html').animate({
+        scrollTop: $('.body-wrapper').offset().top,
+    }, 800)
+
+})
+
+
 $(document).on('scroll', function () {
 
     const scrollvalue = $(this).scrollTop();
@@ -68,20 +80,58 @@ $(document).on('scroll', function () {
         $text2.addClass('active');
     }
 
+
+    //info
+    const $column1 = $('.col1');
+    const column1FromTop = $column1.offset().top;
+    const column1Height = $column1.height();
+
+    const $column2 = $('.col2');
+    const column2FromTop = $column2.offset().top;
+    const column2Height = $column2.height();
+
+    if (scrollvalue > column1FromTop + column1Height / 4 - windowHeight) {
+        $column1.addClass('active');
+    }
+
+    if (scrollvalue > column2FromTop + column2Height / 4 - windowHeight) {
+        $column2.addClass('active');
+    }
+
+
+    //contact
+    const $contact1 = $('.all-contact');
+    const contact1FromTop = $contact1.offset().top;
+    const contact1Height = $contact1.height();
+
+    if (scrollvalue > contact1FromTop + contact1Height / 4 - windowHeight) {
+        $contact1.addClass('active');
+    }
+
     if (scrollvalue < 50) {
         $('div').removeClass('active');
     }
 
 
-
-
-
-
-
-
-
+    if (scrollvalue < 50) {
+        $('div').removeClass('active');
+    }
 
 })
+
+
+//preloader
+var preloaderEl = document.querySelector('#preloader');
+
+window.addEventListener('load', function () {
+    preloaderEl.classList.add('preloader-hiding');
+
+    preloaderEl.addEventListener('transitionend', function () {
+        this.classList.add('preloader-hidden');
+        this.classList.remove('preloader-hiding');
+    });
+});
+
 
 
 //nav
